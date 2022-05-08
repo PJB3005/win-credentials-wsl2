92dc93886c66c47694fdf7a516041a4bd942af83
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-05-08 13:41:07 +0200

    Better build script

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 cl /Fecred_debug.exe /nologo /O2 /W3 cred.c Advapi32.lib kernel32.lib /utf-8 /GS- /MD /link /subsystem:console
 
-cl /Fecred.exe /nologo /O2 /W3 cred.c Advapi32.lib kernel32.lib ucrt.lib /utf-8 /GS- /MD /link /incremental:no /opt:icf /opt:ref /subsystem:console
+cl /Fecred.exe /nologo /O2 /W3 cred.c Advapi32.lib kernel32.lib /utf-8 /GS- /MD /link /incremental:no /opt:icf /opt:ref /subsystem:console
+cl /Fecred_static.exe /nologo /O2 /W3 cred.c Advapi32.lib kernel32.lib /utf-8 /GS- /MT /link /incremental:no /opt:icf /opt:ref /subsystem:console
